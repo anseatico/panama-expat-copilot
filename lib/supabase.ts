858f1b48f5bbb8b39556a3cdd81f740b40a440de
anseatico <cegr1@mac.com>
2026-08-06 17:35:46 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,12 @@
-import { createClient } from "@supabase/supabase-js"; const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || ""; const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""; export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Cliente para el navegador
+export const createClientSupabase = () =>
+  createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Cliente genérico
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
